ab6f20a75cc53266b16cab108eff5cbd695c9d30
Haim Zisman <haimzismann@gmail.com>
2019-10-08 18:49:59 +0300

    Maven Course

---
+++ mb2g-lombok/src/main/java/Model/User.java
@@ -0,0 +1,16 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private String firstname;
+    private String lastname;
+    private String email;
+}
